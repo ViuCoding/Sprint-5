372e38b2152aabbe05e2e3e249eda8bc55ebca6c
Vincenzo- <viulenzgg@gmail.com>
2022-06-17 19:48:03 +0200

    exercise 1 done, will merge to main later

--- ts/app.ts
+++ ts/app.ts
@@ -2,8 +2,13 @@
 const DAD_JOKES = "https://icanhazdadjoke.com/";
 const CHUCK_NORRIS = "https://api.chucknorris.io/jokes/random";
 
-function getJoke() {
+async function getJoke() {
+  const request = await fetch(`${DAD_JOKES}`, {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const data = await request.json();
 
-    
-
+  document.querySelector("#joke")!.innerHTML = data.joke;
 }
