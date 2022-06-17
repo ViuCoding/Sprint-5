d62a96ef3c12f9d76a10ac91f3b4243adbfe2404
Vincenzo- <viulenzgg@gmail.com>
2022-06-17 19:17:52 +0200

    added main function

--- js/app.js
+++ js/app.js
@@ -2,3 +2,5 @@
 // APIs URLs
 const DAD_JOKES = "https://icanhazdadjoke.com/";
 const CHUCK_NORRIS = "https://api.chucknorris.io/jokes/random";
+function getJoke() {
+}
--- ts/app.ts
+++ ts/app.ts
@@ -2,3 +2,8 @@
 const DAD_JOKES = "https://icanhazdadjoke.com/";
 const CHUCK_NORRIS = "https://api.chucknorris.io/jokes/random";
 
+function getJoke() {
+
+    
+
+}
